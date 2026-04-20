f4f4a707126258ba98bcb3fb436da49aa1f54665
Manuel Calixto <calixtopinar@gmail.com>
2026-04-20 12:53:07 +0200

    Estructuración de código

--- config/config.php
+++ config/config.php
@@ -1,10 +1,3 @@
 <?php
 
-const BASE_URL = "http://localhost/AGENDA/";
-
-// Constantes de base de datos desde .env
-const DB_SERVIDOR = 'localhost';
-const DB_USUARIO = 'root';
-const DB_PASS = 'root';
-const DB_DATABASE = 'agenda';
-const DB_CHARSET = 'utf8mb4';
+const BASE_URL = "http://localhost/NextLevelHub/";
